e67c29cb4b03e5a5cd5671233e73cc28c8278bef
Victoriya <vikbelec111@yandex.ru>
2021-07-10 21:38:56 +0300

    feat: add code from the lecture

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
-
+import ServiceAdd from './components/ServiceAdd';
+import ServiceList from './components/ServiceList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+       <ServiceAdd />
+       <ServiceList />
     </div>
   );
 }
---
+++ my-app/src/components/ServiceAdd.js
@@ -0,0 +1,29 @@
+import {useSelector, useDispatch} from 'react-redux';
+import {changeServiceField, addService} from '../actions/actionCreators';
+
+const selectServiceAdd = (state) => state.serviceAdd;
+
+function ServiceAdd() {
+	const item = useSelector(selectServiceAdd);
+	const dispatch = useDispatch();
+
+	const handleChange = evt => {
+		const {name, value} = evt.target;
+		dispatch(changeServiceField(name, value));
+	}
+
+	const handleSubmit = evt => {
+			evt.preventDefault();
+			dispatch(addService(item.name, item.price));
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input name='name' onChange={handleChange} value={item.name} />
+			<input name='price' onChange={handleChange} value={item.price} />
+			<button type='submit'>Save</button>
+		</form>
+	);
+}
+
+export default ServiceAdd;
